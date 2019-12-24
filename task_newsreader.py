159ab603a85fbac14194f3219c6ccccbeb8151db
Yujin <jelec69@naver.com>
2019-12-25 03:55:37 +0900

    making dir

--- task_newsreader.py
+++ task_newsreader.py
@@ -1,34 +1,40 @@
 import re
+import os
 import urllib.request
 from bs4 import BeautifulSoup
 
+url='https://news.v.daum.net/v/20191223235700032'
+html = urllib.request.urlopen(url)
 
-test_url='https://news.v.daum.net/v/20191224215601536'
-html = urllib.request.urlopen(test_url)
+# making file and directory
+dirnam = url.strip('https://news.v.daum.net/v/')
+dirnam = 'C:/Users/USER/田유진/(수업) 자료전산처리방법론/파이썬 과제/newsreader/' + dirnam[:8]
+urlname = url[-5:]
 
-urlname = test_url[-5:]
+filename = dirnam + '/' + urlname + '.txt'
+os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-f = open(urlname+'.txt', 'w', encoding='UTF8')
+f = open(filename, 'w', encoding='UTF8')
 
-
+# read contents
 bs_object = BeautifulSoup(html.read(), 'html.parser')
 title = bs_object.find('h3',{'class':"tit_view"})
 info = bs_object.find('span',{'class':'info_view'})
 summary = bs_object.find('strong', {'class':'summary_view'})
 
-
 news_head = [title, info, summary]
 
-f.write('\n<head>\n')
+# write head part
+f.write('<head>\n')
 for k in news_head:
     k = str(k)
     k = re.sub(r"<[^>]*>", " ", k)
-    k = k + '\n'
-    f.write(k)
+    if k != 'None':
+        k = k + '\n'
+        f.write(k)
 
-
+# write body part
 body = bs_object.find_all('div', {'id':'harmonyContainer'})
-
 
 body = str(body)
 body = body[1:len(body)-1]
@@ -41,9 +47,9 @@ for k in body:
 
 bodyresult = '\n\n'.join(bodylist)
 
-f.write('\n<body>\n')
+f.write('\n<body>')
 f.write(bodyresult)
 
-
+# finishing
 f.close()
 print('done')
